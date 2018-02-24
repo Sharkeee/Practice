606fa73c368951cd1021a2474e1e7a356ee83774
Sharkeee <sharkmen1996@gmail.com>
2018-02-24 21:39:30 +0100

    test

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        if ("Cz     esc!".equalsIgnoreCase("c2zesc!")) {
+            System.out.println("[01] Test passed.");
+        } else {
+            System.out.println("[01] Test failed");
+            String s = "        CzeeS I  E M  A                ";
+            System.out.println(s);
+            s.trim();
+            System.out.println(s);asdasd
+        }
+    }
+}
