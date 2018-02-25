2d12c415c009503a49809b99a898d25222828ce7
Sharkeee <sharkmen1996@gmail.com>
2018-02-25 18:21:10 +0100

    obiekt

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,7 @@ import java.util.Scanner;
 public class Main {
 
     static Scanner in = new Scanner(System.in);
-
     public static void main(String[] args) {
-        System.out.println("Podaj swoje imie: ");
-        String name = in.nextLine();
-        wyborImienia:
-        if (!name.equalsIgnoreCase("przemek")) {
-            System.out.println("ZLE");
-            break wyborImienia;
-        } else {
-            System.out.println("Dobrze");
-            int[] counting = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            System.out.println(Arrays.toString(counting));
-
-            int[] copiedCounting = new int[10];
-            Arrays.fill(copiedCounting, 5);
-            for(int i = 0; i<copiedCounting.length; i++) {
-                System.out.println(copiedCounting[i]);
-
-            }
-        }
+        
     }
 }
