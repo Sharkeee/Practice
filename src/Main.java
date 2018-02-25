cea468f8dc2ac6747b0b4087160864aa4517a956
Sharkeee <sharkmen1996@gmail.com>
2018-02-25 15:36:04 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    Scanner in = new Scanner(System.in);
-    public static void main(String[] args) {
+    static Scanner in = new Scanner(System.in);
 
-        if ("Cz     esc!".equalsIgnoreCase("c2zesc!")) {
-            System.out.println("[01] Test passed.");
-        } else {
-            System.out.println("[01] Test failed");
-            String s = "        CzeeS I  E M  A                ";
-            System.out.println(s);
-            s.trim();
-            System.out.println(s);asdasd
+    public static void main(String[] args) {
+        String imiona[] = {"Przemek", "Tomek", "Adrian", "Jan", "Kurwa", "Igor", "Szymon", "Jakub", "Pawel", "Tymon"};
+        boolean correct = true;
+        while (correct) {
+            for (int i = 0; i < 9; i++) {
+                System.out.println("Czy Twoje imie to " + imiona[i]);
+                String answer = in.next();
+                if (answer.equalsIgnoreCase("yes")) {
+                    correct = false;
+                } else {
+                    correct = true;
+                }
+                if (correct = false) {
+                    break;
+                }
+            }
         }
     }
 }
