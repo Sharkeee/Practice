872fa0ff2621471334dd3754c00eeb72af7289f2
Sharkeee <sharkmen1996@gmail.com>
2018-02-25 15:48:24 +0100

    Etykiety

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,16 @@ public class Main {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String imiona[] = {"Przemek", "Tomek", "Adrian", "Jan", "Kurwa", "Igor", "Szymon", "Jakub", "Pawel", "Tymon"};
-        boolean correct = true;
-        while (correct) {
-            for (int i = 0; i < 9; i++) {
-                System.out.println("Czy Twoje imie to " + imiona[i]);
-                String answer = in.next();
-                if (answer.equalsIgnoreCase("yes")) {
-                    correct = false;
-                } else {
-                    correct = true;
-                }
-                if (correct = false) {
-                    break;
-                }
-            }
+        System.out.println("Podaj swoje imie: ");
+        String name = in.nextLine();
+        wyborImienia:
+        if(!name.equalsIgnoreCase("przemek")) {
+            System.out.println("ZLE");
+            break wyborImienia;
         }
+        else {
+            System.out.println("Dobrze");
+        }
+
     }
 }
