3c25281ac07169451b97d0a2c6612dc36cce7ffe
Sharkeee <sharkmen1996@gmail.com>
2018-02-27 18:26:12 +0100

    raise method

--- src/Employee.java
+++ src/Employee.java
@@ -1,15 +1,16 @@
+import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Employee {
+    static Scanner in = new Scanner(System.in);
     private String name;
     private double salary;
     private LocalDate hireDay;
     private static int nextId = 1;
     private int id;
 
-    Scanner in = new Scanner(System.in);
 
     public String getName() {
         return name;
@@ -38,33 +39,24 @@ public class Employee {
 
 
     }
+
     public void setId() {
         id = nextId;
         nextId++;
     }
 
-
-    public void raiseSalary(Employee pracownik, Double newSalary) {
-        if (newSalary > pracownik.salary) {
-            System.out.println("Czy jestes pewien ze chcesz zwiekszyc pensje pracownika " + pracownik.getName() + " o " + (newSalary - pracownik.salary));
-            String answer = in.next();
-            if (answer.equalsIgnoreCase("tak")) {
-                pracownik.salary = newSalary;
-                System.out.println("Pensja pomyslnie podwyzszona");
-            } else {
-                System.out.println("Pensja nie zostala podniesiona");
-
-            }
-        } else {
-            System.out.println("Czy chcesz obnizyc pensje pracownika " + pracownik.getName() + " o " + (pracownik.salary - newSalary));
-            String answer = in.next();
-            if (answer.equalsIgnoreCase("tak")) {
-                pracownik.salary = newSalary;
-                System.out.println("Pensja pomyslnie obnizona");
-            } else {
-                System.out.println("Pensja nie zostala obnizona");
-
-            }
+    public void przyznajPremie(Double premia) {
+        System.out.println("Planujesz wlasnie przyznac " + premia + "PLN premii pracownikowi o ID [" + this.getId()+"]");
+        ;
+        System.out.println("Aby zatwierdzic przyznanie premii wpisz TAK");
+        String answer = in.next();
+        if(answer.equalsIgnoreCase("tak")) {
+            this.salary += premia;
+            System.out.println("Premia przyznana, obecne zarobki pracownika ("+this.getId()+") wynosza " + this.getSalary() + "PLN");
         }
+        else {
+            System.out.println("Przyznawanie premii anulowane");
+        }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -6,12 +7,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Employee dev = new Employee("Dev", 1337.0,2018,1,1);
         Employee boss = new Employee("Kijak",1980.0,2018,1,15);
         Employee[] staff = new Employee[2];
-        staff[0] = new Employee("Grudzinski",1500.0,2018,2,25);
+        staff[0] = new Employee("Kowalski",1500.0,2018,2,25);
         staff[1] = new Employee("Mazur",300.0,2018,1,15);
 
-
+        dev.setId();
         boss.setId();
         staff[0].setId();
         staff[1].setId();
@@ -19,6 +21,10 @@ public class Main {
         System.out.println(staff[0].getId());
         System.out.println(staff[1].getId());
         System.out.println(Employee.getNextId());
+        Double x = 0.1;
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+        System.out.println(currencyFormatter.format(x));
+        dev.przyznajPremie(500.0);
     }
 
 }
