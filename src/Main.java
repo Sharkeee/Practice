f70c97e78acba7c442e470c65bec391ae12fceec
Sharkeee <sharkmen1996@gmail.com>
2018-03-21 20:26:13 +0100

    array list testing

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,8 @@ public class Main {
 
 
         ArrayList<Employee> staff = new ArrayList<>();
-        staff.add(new Employee("List testowy",1500.0,2018,1,13));
-        staff.add(new Employee("Tester listowy", 3000.0, 2018,3,15));
+        staff.add(new Employee("List testowy", 1500.0, 2018, 1, 13));
+        staff.add(new Employee("Tester listowy", 3000.0, 2018, 3, 15));
         staff.ensureCapacity(100);
         // abstrakcyjne
         Person p = new Student("Tomasz", "Programowanie");
@@ -23,10 +23,14 @@ public class Main {
         people[1] = new Student("Przemek Test", "Informatyka");
 
         System.out.println(people[0].getDescription());
-        System.out.println(staff.size());
+        System.out.println("Pobranie informacji o pracowniku z array list (TEST): \n");
+        for(int i = 0; i<+staff.size(); i++) {
+            System.out.println("Dane osobowe: " + staff.get(i).getName());
+            System.out.println("Zarobki: " + staff.get(i).getSalary());
+            System.out.println("Data zatrudnienia: " + staff.get(i).getHireDay());
+            System.out.println("\n");
+            
+        }
     }
-
-
-
 }
 
